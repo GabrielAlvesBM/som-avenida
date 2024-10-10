300dbeffeb3879144f096b1dd8c6f21db4551193
Gabriel Alves <gabrielalves6002@gmail.com>
2024-10-10 15:19:00 -0300

    style: ads and comment

--- src/components/AdsRight.tsx
+++ src/components/AdsRight.tsx
@@ -2,20 +2,20 @@ import Image from 'next/image';
 
 const AdsRight = () => {
     return (
-        <div className="grid gap-0.5 w-full max-w-[250px] p-1 mx-auto bg-adsBg">
-            <Image className='w-full'
+        <div className="grid grid-cols-2 gap-1.5 w-full p-1 mx-auto bg-adsBg md:max-w-[250px] md:grid-cols-none">
+            <Image className='w-full max-w-[250px] md:max-w-none'
                 src="/images/ads-gildogas.jpeg"
                 alt="Ads Do Gil do Gas"
                 width={250}
                 height={150}
             />
-            <Image className='w-full'
+            <Image className='w-full max-w-[250px] md:max-w-none'
                 src="/images/ads-cafeluana.png"
                 alt="Ads Do Café Luana"
                 width={250}
                 height={150}
             />
-            <Image className='w-full'
+            <Image className='col-span-2 mx-auto w-full max-w-[250px] md:max-w-none'
                 src="/images/ads-renovo.jpeg"
                 alt="Ads Do Restaurante Renovo"
                 width={250}
--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -16,8 +16,8 @@ export default function Comment({ name, content, date, hour}: CommentProps) {
     return (
         <article className="m-3 bg-myLightBlack">
             <div className="flex justify-between pt-[10px] mx-4 mb-5">
-                <h3 id="name" className={`${montserrat.className} text-xl text-base`}>{ name}</h3>
-                <span className="my-auto text-sm text-myLightGray">
+                <h3 id="name" className={`${montserrat.className} text-base sm:text-xl`}>{ name}</h3>
+                <span className="my-auto text-xs text-myLightGray sm:text-sm">
                     <time id="date" dateTime={ date } className="mr-1">{ date }</time>
                     <time id="hour" dateTime={ hour }>{ hour }</time>
                 </span>
