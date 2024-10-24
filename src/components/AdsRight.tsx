da28690a5ad546f1874920d1bb42845e076ccb9f
Gabriel Alves <gabrielalves6002@gmail.com>
2024-10-24 18:01:13 -0300

    add(apae): new add

--- src/components/AdsRight.tsx
+++ src/components/AdsRight.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image';
 
 const AdsRight = () => {
     return (
-        <div className="grid grid-cols-2 gap-1.5 w-full p-1 mx-auto bg-adsBg md:max-w-[250px] md:ml-2 md:grid-cols-none">
+        <div className="grid grid-cols-2 gap-2 w-full p-1 mx-auto bg-adsBg md:max-w-[250px] md:ml-2 md:grid-cols-none">
+            
             <Image className='w-full max-w-[250px] md:max-w-none'
                 src="/images/ads-gildogas.jpeg"
                 alt="Ads Do Gil do Gas"
@@ -18,6 +19,12 @@ const AdsRight = () => {
             <Image className='col-span-2 mx-auto w-full max-w-[250px] md:max-w-none md:col-span-full'
                 src="/images/ads-renovo.jpeg"
                 alt="Ads Do Restaurante Renovo"
+                width={250}
+                height={150}
+            />
+            <Image className='col-span-2 mx-auto w-full max-w-[250px] md:max-w-none md:col-span-full'
+                src="/images/ads-apae.jpeg"
+                alt="Ads Da Apae"
                 width={250}
                 height={150}
             />
