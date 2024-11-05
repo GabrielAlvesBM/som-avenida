446d7d40182243274def8e8c7168b927c6ab9931
GabrielAlvesBM <gabrielalves6002@gmail.com>
2024-11-05 15:22:47 -0300

    feat(AdsTop): exchange images

--- src/components/AdsTop.tsx
+++ src/components/AdsTop.tsx
@@ -1,22 +1,51 @@
+'use client'
+
+import { useState } from 'react';
 import Image from 'next/image';
 
-const AdsRight = () => {
+const AdsTop = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const images = [
+        '/images/ads-braseiro.png',
+        '/images/ads-paguemenos.jpg',
+    ];
+
+    const showImage = (index: number) => {
+        if (index >= images.length) {
+          setCurrentIndex(0);
+        } else if (index < 0) {
+          setCurrentIndex(images.length - 1);
+        } else {
+          setCurrentIndex(index);
+        }
+      };
+
+      const nextImage = () => showImage(currentIndex + 1);
+      const prevImage = () => showImage(currentIndex - 1);
+
     return (
-        <div className="w-full max-w-[1000px] md:mx-auto">
-            <Image className='w-full rounded-xl'
-                src="/images/ads-braseiro.png"
-                alt="Ads Do Braseiro"
-                width={1500}
-                height={150}
-            />
-            <Image className='hidden w-full'
-                src="/images/ads-paguemenos.jpg"
-                alt="Ads Do Pague Menos"
-                width={325}
-                height={150}
-            />
-        </div>
+        <div className="relative w-full max-w-[1000px] rounded-xl overflow-hidden md:mx-auto">
+            <button className="absolute top-1/2 left-2 transform -translate-y-1/2 
+            bg-blackBase opacity-90 text-whiteBase p-2 rounded-full z-10" onClick={prevImage}>❮</button>
+
+            <div className="flex transition-transform ease-out duration-500" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                {images.map((src, index) => (
+                <div className="min-w-full" key={index}>
+                    <Image className='w-full rounded-xl'
+                        src={src} 
+                        alt={`Imagem ${index + 1}`} 
+                        width={1000}
+                        height={250}
+                    />
+                </div>
+                ))}
+            </div>
+
+            <button className="absolute top-1/2 right-2 transform -translate-y-1/2
+            bg-blackBase opacity-90 text-whiteBase p-2 rounded-full z-10" onClick={nextImage}>❯</button>
+        </div>   
     );
 };
 
-export default AdsRight;
+export default AdsTop;
